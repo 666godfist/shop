0d3f758429e78cc970c40a2ec746b7ca4cf5227d
Paweł <113582471+666godfist@users.noreply.github.com>
2022-11-30 15:38:31 +0100

    zmiany
    zmiany

--- index.php
+++ index.php
@@ -7,13 +7,31 @@
     <title>Sklep</title>
 </head>
 <body>
-    <?php
+<?php
+        $con = new mysqli("127.0.0.1","root","","skelp-zsp");
+        echo '<form method="POST">';
+        $res = $con->query("SELECT * FROM users");
+        $cos = $res->fetch_all();
 
+        echo '<center><div class="d1"><h1>Logowanie:</h1><br> Nazwa Użytkownika: <input name="login"><br> Haslo: <input name="password" type="password"><br><input type="submit">';
+        if($_POST!=NULL)
+        {
+            for($i=0;$i<count($cos);$i++)
+            {
+                if($_POST['login']==$cos[$i][1] && $_POST['password']==$cos[$i][2])
+                {
+                    echo 'udalo sie zalogowac';
+                    header('location: strona.php');
+                    break;
+                }
+                else
+                {
+                }
+            }
+        }
+        echo '</form>';
+
+        echo '<form action="rejestracja.php"><button>Rejestracja</button></form></center></div>';
     ?>
-    <form method="POST" action="login.php">
-        <input type="Login" name="Login" placeholder="Login" required>
-        <input type="password" name="password" placeholder="Hasło" required>
-        <input type="submit" name="submit" value="Wyślij">
-    </form>
 </body>
 </html>
